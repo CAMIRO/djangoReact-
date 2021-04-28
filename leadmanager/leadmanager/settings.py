cab9e7a035b81d4dbea00fe9fcb1111f36209eb8
CAMIRO <juancamilosc@hotmail.com>
2021-04-28 14:55:21 +0800

    protecting key values

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eal$#hmlq=_2!xcin_5t2zl*f&j*b$=2!d$$=2!&=5w*8lngd8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
