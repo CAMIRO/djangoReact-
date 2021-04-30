4337008a6cd4f3cc37d2097750a342e3315798bc
CAMIRO <juancamilosc@hotmail.com>
2021-04-30 14:48:04 +0800

    React.js added to the project

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+// Components
+import Header from "./layout/Header";
+import Dashboard from "./leads/Dashboard";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+import Leads from "./Leads";
+const Dashboard = (props) => {
+  return (
+    <>
+      <Form />
+      <Leads />
+    </>
+  );
+};
+
+export default Dashboard;
