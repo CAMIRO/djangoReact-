477f26344f9e688c54c4a7bd18daef32237c63b6
CAMIRO <juancamilosc@hotmail.com>
2021-05-10 11:41:16 +0800

    adding the delete feature

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 // actions
-import { getLeads } from "../../actions/leads";
+import { getLeads, deleteLead } from "../../actions/leads";
 
 export class Leads extends Component {
   static propTypes = {
@@ -35,7 +35,12 @@ export class Leads extends Component {
                 <td>{lead.email}</td>
                 <td>{lead.message}</td>
                 <td>
-                  <button className="btn btn-danger  btn-sm">Delete</button>
+                  <button
+                    onClick={this.props.deleteLead.bind(this, lead.id)}
+                    className="btn btn-danger  btn-sm"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
@@ -50,4 +55,4 @@ const mapStateToProps = (state) => ({
   leads: state.leads.leads,
 });
 
-export default connect(mapStateToProps, { getLeads })(Leads);
+export default connect(mapStateToProps, { getLeads, deleteLead })(Leads);
