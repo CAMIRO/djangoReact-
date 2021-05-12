705b51149acbf347cc4d5fff93b24b5b3a2be4c5
CAMIRO <juancamilosc@hotmail.com>
2021-05-12 15:24:33 +0800

    including react-router frontend

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 // Alerts
 import { Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 // Components
 import Header from "./layout/Header";
-import Dashboard from "./leads/Dashboard";
 import Alerts from "./layout/Alerts";
+// Views
+import Dashboard from "./leads/Dashboard";
+import Login from "./accounts/Login";
+import Register from "./accounts/Register";
 // Redux
 import { Provider } from "react-redux";
 import store from "../store";
@@ -22,11 +31,17 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Header />
-          <Alerts />
-          <div className="container">
-            <Dashboard />
-          </div>
+          <Router>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/register" component={Register} />
+              </Switch>
+            </div>
+          </Router>
         </AlertProvider>
       </Provider>
     );
