f3d8303cf453ac4237d12964c89ad460e0c85d74
CAMIRO <juancamilosc@hotmail.com>
2021-05-22 14:03:30 +0800

    integratring db into posgres once more

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoDB',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
